bcd2634bdc45a86770cec911995daa51631ac2b5
Ronny Friedland <mail@ronnyfriedland.de>
2022-01-08 20:11:49 +0100

    Introduce archunit
    - add first tests
    
    Addresses: #27

---
+++ adr-maven-plugin/src/test/java/archunit/ArchitectureTest.java
@@ -0,0 +1,56 @@
+package archunit;
+
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import de.ronnyfriedland.adr.export.exception.ExportProcessorException;
+import de.ronnyfriedland.adr.template.TemplateProcessor;
+import freemarker.template.Template;
+import org.apache.maven.plugins.annotations.Mojo;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.plantuml.PlantUmlArchCondition.Configurations.*;
+import static com.tngtech.archunit.library.plantuml.PlantUmlArchCondition.adhereToPlantUmlDiagram;
+
+/**
+ * @author ronnyfriedland
+ */
+@AnalyzeClasses(packages = "de.ronnyfriedland.adr", importOptions = {ImportOption.DoNotIncludeTests.class})
+public class ArchitectureTest {
+
+    @ArchTest
+    public static final ArchRule ruleMojo = classes()
+            .that().areAnnotatedWith(Mojo.class)
+            .should().haveSimpleNameEndingWith("Mojo");
+
+    @ArchTest
+    public static final ArchRule templatePackageRule = classes()
+            .that().areAssignableTo(TemplateProcessor.class)
+            .should().resideInAPackage("de.ronnyfriedland.adr.template");
+
+    @ArchTest
+    public static final ArchRule formatSpecificExporterPackageRule = classes()
+            .that().haveSimpleNameStartingWith("Export")
+            .and().doNotHaveSimpleName("ExportAdrMojo")
+            .and().doNotHaveSimpleName("ExportProcessorException")
+            .and().doNotHaveSimpleName("ExportProcessor")
+            .should().resideInAPackage("de.ronnyfriedland.adr.export.formats");
+
+    @ArchTest
+    public static final ArchRule mojoPackageRule = classes()
+            .that().haveSimpleNameEndingWith("Mojo")
+            .should().resideInAPackage("de.ronnyfriedland.adr");
+
+    @ArchTest
+    public static final ArchRule exceptionsPackageRule = classes()
+            .that().areAssignableTo(Exception.class)
+            .should().resideInAPackage("..exception");
+
+    @ArchTest
+    public static final ArchRule enumsPackageRule = classes()
+            .that().areAssignableTo(Enum.class)
+            .should().resideInAPackage("..enums");
+}
+
+
